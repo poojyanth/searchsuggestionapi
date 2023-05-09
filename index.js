d75d16e006f67dd478ee01258e787c725bfa7682
poojyanth <96917000+poojyanth@users.noreply.github.com>
2023-05-09 20:30:38 +0530

    chanegs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/suggest/:q', (req, res) => {
     https.get(`https://www.google.com/complete/search?client=chrome&q=${req.params.q}`, (resp) => {
